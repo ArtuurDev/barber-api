c773aeee4abbbe0eaa75f20c68c853e548bf917d
artuur <arturcastrodossantos.com@gmail.com>
2025-03-11 14:32:03 -0300

    unique-entity-id

---
+++ src/domain/@core/entitys/unique-entity-id.ts
@@ -0,0 +1,15 @@
+import { randomUUID } from "crypto"
+
+export class UniqueEntityId {
+
+    private _value: string
+
+    constructor(value?: string) {
+        this._value = value ?? randomUUID()
+    }
+
+    toValue() {
+        return this._value
+    }
+
+}
