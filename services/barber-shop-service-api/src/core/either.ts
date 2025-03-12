b11597b81f00bd2d7ff585df2db387f41d4f0f2d
artuur <arturcastrodossantos.com@gmail.com>
2025-03-12 17:22:42 -0300

    either

---
+++ services/barber-shop-service-api/src/core/either.ts
@@ -0,0 +1,46 @@
+export class Left<L,R> {
+    readonly value: any
+
+    constructor(value: any) {
+        this.value = value
+    }
+
+    isRight(): this is Right<L,R> {
+        return false
+    }
+
+    isLeft(): this is Left<L,R> {
+        return true
+    }
+
+}
+
+export class Right<L,R> {
+
+    readonly value: any
+
+    constructor(value: any) {
+        this.value = value
+    }
+
+    isRight(): this is Right<L,R> {
+        return true
+    }
+
+    isLeft(): this is Left<L,R> {
+        return false
+    }
+}
+
+export const left = <L,R>(value: L): Either<L,R> => {
+
+    return new Left(value)
+
+}
+
+export type Either<L,R> = Left<L,R> | Right<L,R>
+
+export const right = <L,R>(value: R): Either<L,R> => {
+    
+    return new Right(value)
+}
