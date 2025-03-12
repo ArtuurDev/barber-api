f272735ceb38ff7918f2d3cec2829409d22b69fb
artuur <arturcastrodossantos.com@gmail.com>
2025-03-12 18:05:22 -0300

    refatoração

---
+++ services/barber-shop-service-api/src/domain/application/use-cases/create-client-use-case.spec.ts
@@ -0,0 +1,123 @@
+import { beforeEach, describe, expect, it } from "vitest"
+import { CreateClientUseCase } from "./create-client-use-case"
+import { ClientInMemoryRepository } from "../../../test/repositories/client-in-memory-repository"
+import { PhoneFormatIncorretly } from "../../../errors/phone-format-incorretly"
+import { CpfFormatIncorretly } from "../../../errors/cpf-format-incorretly"
+import { PasswordFormatIncorretly } from "../../../errors/password-format-incorretly"
+import { CpfDuplicate } from "../../../errors/cpf-duplicate"
+import { EmailDuplicate } from "../../../errors/email-duplicate"
+
+describe('Tests related to creating a client and value objects of Client',  () => {
+    let inMemoryClientRepository: ClientInMemoryRepository
+    let sut: CreateClientUseCase
+
+    beforeEach(() => {
+        inMemoryClientRepository = new ClientInMemoryRepository()
+        sut = new CreateClientUseCase(inMemoryClientRepository)
+    }) 
+
+    it('Should not be possible to register a customer with a duplicate CPF', async () => {
+
+        const result = await sut.execute({
+            name: 'artur',
+            email: 'arturcastrodossantos.com@gmail.com',
+            password: '1234Am@',
+            cpf: '622.408.473-44',
+            phone: '88996036330',
+            birthDateAt: new Date()        
+        })  
+
+        const result2 = await sut.execute({
+            name: 'artur',
+            email: 'arturcastrodossantos.com@gmail.com',
+            password: '1234Am@',
+            cpf: '622.408.473-44',
+            phone: '88996036330',
+            birthDateAt: new Date()        
+        })  
+
+        expect(result2.isLeft()).toBe(true)
+        expect(result2.value).toBeInstanceOf(CpfDuplicate)
+
+    })
+    it('Should not be possible to register a customer with a duplicate Email', async () => {
+
+        const result = await sut.execute({
+            name: 'artur',
+            email: 'arturcastrodossantos.com@gmail.com',
+            password: '1234Am@',
+            cpf: '622.408.473-44',
+            phone: '88996036330',
+            birthDateAt: new Date()        
+        })  
+
+        const result2 = await sut.execute({
+            name: 'artur',
+            email: 'arturcastrodossantos.com@gmail.com',
+            password: '1234Am@',
+            cpf: '622.408.473-22',
+            phone: '88996036330',
+            birthDateAt: new Date()        
+        })  
+
+        expect(result2.isLeft()).toBe(true)
+        expect(result2.value).toBeInstanceOf(EmailDuplicate)
+
+    })
+
+   it('Shoul be able to create client', async () => {
+
+    const result = await sut.execute({
+        name: 'artur',
+        email: 'arturcastrodossantos.com@gmail.com',
+        password: '1234Am@',
+        cpf: '622.408.473-44',
+        phone: '88996036330',
+        birthDateAt: new Date()        
+    })  
+    expect(result.isRight()).toBe(true)
+    expect(inMemoryClientRepository.items).length(1)
+   })
+
+   it('CPF must XXX.XXX.XXX-XX', async () => {
+
+    const result = await sut.execute({
+        name: 'artur',
+        email: 'arturcastrodossantos.com@gmail.com',
+        password: '1234Am@',
+        cpf: '',      
+        phone: '88996036330',
+        birthDateAt: new Date()        
+    })
+    expect(result.isLeft()).toBe(true)
+    expect(result.value).toBeInstanceOf(CpfFormatIncorretly)
+
+   })
+   it('Should not be possible to create a customer with a number that does not have 11 digits', async () => {
+
+    const result = await sut.execute({
+        name: 'artur',
+        email: 'arturcastrodossantos.com@gmail.com',
+        password: '1234Am@',
+        cpf: '622.408.473-44',      
+        phone: '8899603633',
+        birthDateAt: new Date()        
+    })
+    expect(result.isLeft()).toBe(true)
+    expect(result.value).toBeInstanceOf(PhoneFormatIncorretly)
+   })
+   it('Not should be able an possible to create client can with password unformated', async () => {
+
+    const result = await sut.execute({
+        name: 'artur',
+        email: 'arturcastrodossantos.com@gmail.com',
+        password: '123456',
+        cpf: '622.408.473-44',      
+        phone: '8899603633',
+        birthDateAt: new Date()        
+    })
+    expect(result.isLeft()).toBe(true)
+    expect(result.value).toBeInstanceOf(PasswordFormatIncorretly)
+   })
+
+})
