9bd1d822022211e7c849a10b1ea3da87753d81c6
artuur <arturcastrodossantos.com@gmail.com>
2025-03-12 17:25:45 -0300

    repositorio em memorria

---
+++ services/barber-shop-service-api/src/test/repositories/client-in-memory-repository.ts
@@ -0,0 +1,31 @@
+import { ClientRepository } from "../../domain/application/repositories/client-repositorie"
+import { Client } from "../../domain/enterprise/entities/client"
+
+export class ClientInMemoryRepository implements ClientRepository{
+
+    public items: Client[] = [] 
+
+    async create(client: Client): Promise<any>  {
+    
+        return this.items.push(client)
+        
+    }
+
+    async findByEmail(email: string): Promise<Client | undefined> {
+        
+        const isValidatedCpf = this.items.find(item => item.props.email === email) 
+        
+        return isValidatedCpf
+
+    }
+
+    async findByCpf(cpf: string): Promise<Client | undefined> {
+        
+        const isValidatedCpf = this.items.find(item => item.props.cpf === cpf) 
+        
+        return isValidatedCpf
+
+    }
+    
+    
+}
