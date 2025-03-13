a1dba850649d0706547486309ec7f943b6675a52
artuur <arturcastrodossantos.com@gmail.com>
2025-03-13 17:51:29 -0300

    metodo save

--- services/barber-shop-service-api/src/test/repositories/client-in-memory-repository.ts
+++ services/barber-shop-service-api/src/test/repositories/client-in-memory-repository.ts
@@ -1,3 +1,4 @@
+import { UniqueEntityId } from "../../core/entitys/unique-entity-id"
 import { ClientRepository } from "../../domain/application/repositories/client-repositorie"
 import { Client } from "../../domain/enterprise/entities/client"
 
@@ -17,26 +18,37 @@ export class ClientInMemoryRepository implements ClientRepository{
 
     async findById(id: string): Promise<Client | undefined> {
         
-        const client = this.items.find(item => item.id.toValue === id)
+        const client = this.items.find(item => item._id.toValue === id)
         return client
     }
 
     async findByEmail(email: string): Promise<Client | undefined> {
         
-        const isValidatedCpf = this.items.find(item => item.props.email === email) 
+        const isValidatedCpf = this.items.find(item => item.email === email) 
         return isValidatedCpf
 
     }
 
     async findByCpf(cpf: string): Promise<Client | undefined> {
         
-        const isValidatedCpf = this.items.find(item => item.props.cpf === cpf) 
+        const isValidatedCpf = this.items.find(item => item.cpf === cpf) 
         return isValidatedCpf
+    }
+
+    async save(data: Client, id: string): Promise<Client | undefined> {
+        
+        const index = this.items.findIndex(item => item._id.toValue === id) 
+        if(index === -1) {
+            return undefined
+        } 
+
+        return this.items[index] = data
+
     }
 
     async delete(id: string): Promise<Client | undefined> {
         
-        const index = this.items.findIndex(item => item.id.toValue === id)
+        const index = this.items.findIndex(item => item._id.toValue === id)
 
         const itemRemoved = this.items.splice(index, 1)
         
