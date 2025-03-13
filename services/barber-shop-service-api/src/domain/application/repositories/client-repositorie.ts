f2132f4e534f26a4418368c70ef18c5811e07663
artuur <arturcastrodossantos.com@gmail.com>
2025-03-13 17:52:09 -0300

    metodo save

--- services/barber-shop-service-api/src/domain/application/repositories/client-repositorie.ts
+++ services/barber-shop-service-api/src/domain/application/repositories/client-repositorie.ts
@@ -6,6 +6,7 @@ export interface ClientRepository {
     findById(id:string): Promise<Client | undefined>
     findByEmail(email: string): Promise<Client | undefined>
     findByCpf(cpf: string): Promise<Client | undefined>
+    save(data: Client, id: string): Promise<Client | undefined>
     delete(id: string): Promise<Client | undefined>
 
 }
