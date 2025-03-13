8f2925c7527c4dac12da667b40b261c736ea0452
artuur <arturcastrodossantos.com@gmail.com>
2025-03-13 17:50:46 -0300

    algumas alteraçoes

--- services/barber-shop-service-api/src/core/utils/formated-passord.ts
+++ services/barber-shop-service-api/src/core/utils/formated-passord.ts
@@ -1,11 +1,13 @@
+import { PasswordFormatIncorretly } from "../../errors/password-format-incorretly"
+
 export function formatPassord(passord: string) {
 
     if(passord.length < 6) {
-        return null
+        throw new PasswordFormatIncorretly()
     }
     const regex = new RegExp('^(?=.*[a-z])(?=.*[A-Z])(?=.*[^A-Za-z0-9]).*$')
     if(!regex.test(passord)) {
-        return null
+        throw new PasswordFormatIncorretly()
     }
     return passord
 
