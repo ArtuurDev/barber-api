4dd67393eab33ada251969b2450d0d3178014129
artuur <arturcastrodossantos.com@gmail.com>
2025-03-13 17:51:05 -0300

    extendendo de error

--- services/barber-shop-service-api/src/errors/email-format-incorretly.ts
+++ services/barber-shop-service-api/src/errors/email-format-incorretly.ts
@@ -1,9 +1,10 @@
-export class EmailFormatIncorretly  {
+export class EmailFormatIncorretly extends Error {
 
     code?: number 
     message: string
 
     constructor(message?:string, code?:number) {
+        super()
         this.code = code ?? 400
         this.message = message ?? 'E-mail incorrect format'
     } 
