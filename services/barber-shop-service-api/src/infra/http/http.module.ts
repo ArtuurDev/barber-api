637cda4e09e429fdb30821c6f27980f419735c39
artuur <arturcastrodossantos.com@gmail.com>
2025-03-15 15:08:16 -0300

    http module

--- services/barber-shop-service-api/src/infra/http/http.module.ts
+++ services/barber-shop-service-api/src/infra/http/http.module.ts
@@ -1,9 +1,16 @@
 import { Module } from "@nestjs/common";
 import { DatabaseModule } from "../database/database.module";
 import { CreateClientController } from "./controllers/create-client.controller";
+import { ClientRepository } from "../../domain/application/repositories/client-repositorie";
+import { PrismaClientRepository } from "../database/prisma/repositories/prisma-client-repository";
+import { CreateClientUseCase } from "../../domain/application/use-cases/create-client-use-case";
 
 @Module({
     imports: [DatabaseModule],
-    controllers: [CreateClientController]
+    controllers: [CreateClientController],
+    providers: [CreateClientUseCase, {
+        provide: ClientRepository,
+        useClass: PrismaClientRepository
+    }]
 })
 export class HttpModule {}
