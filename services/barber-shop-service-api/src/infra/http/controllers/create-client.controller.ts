ae59a341d69bd2ec9eca522773dce05d89a53410
artuur <arturcastrodossantos.com@gmail.com>
2025-03-15 15:09:06 -0300

    controller de criação de client

--- services/barber-shop-service-api/src/infra/http/controllers/create-client.controller.ts
+++ services/barber-shop-service-api/src/infra/http/controllers/create-client.controller.ts
@@ -1,6 +1,49 @@
-import { Controller } from "@nestjs/common";
+import { Body, Controller, Post, Res, UsePipes } from "@nestjs/common";
+import { CreateClientUseCase } from "services/barber-shop-service-api/src/domain/application/use-cases/create-client-use-case";
+import { z } from "zod";
+import { ZodValidationPipe } from "./pipes/zod-validation-pipe";
+import { Response } from "express";
+
+const requestClientSchema = z.object({
+    name: z.string(),
+    email: z.string().email(),
+    password: z.string(),
+    phone: z.string(),
+    cpf: z.string(),
+    birthDateAt: z.string()
+})
+
+type ClientRequest = z.infer<typeof requestClientSchema>
 
 @Controller()
 export class CreateClientController {
 
+    constructor(
+        private readonly createClientUseCase: CreateClientUseCase
+    ) {}
+
+    @Post('/create')
+    @UsePipes(new ZodValidationPipe(requestClientSchema))
+    async handle(@Body() body: ClientRequest, @Res() res: Response) {
+
+        const {name,email,password,phone,cpf,birthDateAt} = body
+
+        const result = await this.createClientUseCase.execute({
+            birthDateAt,
+            cpf,
+            email,
+            name,
+            password,
+            phone
+        })
+
+        if(result.isLeft()) {
+            return res.status(result.value.code).json(result.value)
+        }
+
+        return res.status(201).json({
+            message: 'success'
+        })
+        
+    }
 }
