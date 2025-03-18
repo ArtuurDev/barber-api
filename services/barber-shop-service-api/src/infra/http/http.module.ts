f6787a81200eb3e87856d2c1c9a92bc139d19477
artuur <arturcastrodossantos.com@gmail.com>
2025-03-18 14:01:27 -0300

    add getclientcontrller

--- services/barber-shop-service-api/src/infra/http/http.module.ts
+++ services/barber-shop-service-api/src/infra/http/http.module.ts
@@ -4,11 +4,13 @@ import { CreateClientController } from "./controllers/clients/create-client.cont
 import { ClientRepository } from "../../domain/application/repositories/client-repositorie";
 import { PrismaClientRepository } from "../database/prisma/repositories/prisma-client-repository";
 import { CreateClientUseCase } from "../../domain/application/use-cases/create-client-use-case";
+import { GetClientsController } from "./controllers/clients/list-clients-controller";
+import { GetClientsUseCase } from "../../domain/application/use-cases/get-clients";
 
 @Module({
     imports: [DatabaseModule],
-    controllers: [CreateClientController],
-    providers: [CreateClientUseCase, {
+    controllers: [CreateClientController, GetClientsController],
+    providers: [CreateClientUseCase, GetClientsUseCase, {
         provide: ClientRepository,
         useClass: PrismaClientRepository
     }]
