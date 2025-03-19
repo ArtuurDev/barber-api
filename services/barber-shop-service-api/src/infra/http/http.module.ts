8cedea6a4b23319e98db7fb06da43ff3d5e81cba
artuur <arturcastrodossantos.com@gmail.com>
2025-03-19 09:37:36 -0300

    add controler de deletar client e provider de caso de uso

--- services/barber-shop-service-api/src/infra/http/http.module.ts
+++ services/barber-shop-service-api/src/infra/http/http.module.ts
@@ -6,13 +6,28 @@ import { PrismaClientRepository } from "../database/prisma/repositories/prisma-c
 import { CreateClientUseCase } from "../../domain/application/use-cases/create-client-use-case";
 import { GetClientsController } from "./controllers/clients/list-clients-controller";
 import { GetClientsUseCase } from "../../domain/application/use-cases/get-clients";
+import { UpdateClientController } from "./controllers/clients/update-clients.controller";
+import { UpdateClientUseCase } from "../../domain/application/use-cases/update-client";
+import { DeleteClientController } from "./controllers/clients/delete-client.controller";
+import { DeleteClientsUseCase } from "../../domain/application/use-cases/delete-client";
 
 @Module({
     imports: [DatabaseModule],
-    controllers: [CreateClientController, GetClientsController],
-    providers: [CreateClientUseCase, GetClientsUseCase, {
+    controllers: [
+    CreateClientController, 
+    GetClientsController, 
+    UpdateClientController,
+    DeleteClientController
+],
+    providers: [
+        CreateClientUseCase, 
+        GetClientsUseCase, 
+        UpdateClientUseCase,
+        DeleteClientsUseCase, 
+        {
         provide: ClientRepository,
         useClass: PrismaClientRepository
-    }]
+        }
+    ]
 })
 export class HttpModule {}
