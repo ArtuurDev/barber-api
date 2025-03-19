b958905d5f4daf9889323c777a81b5777e742836
artuur <arturcastrodossantos.com@gmail.com>
2025-03-19 09:35:59 -0300

    modificando retorno do metodo delete

--- services/barber-shop-service-api/src/domain/application/repositories/client-repositorie.ts
+++ services/barber-shop-service-api/src/domain/application/repositories/client-repositorie.ts
@@ -8,6 +8,6 @@ export abstract class ClientRepository {
     abstract findByPhone(phone: string): Promise<Client | null>
     abstract findByCpf(cpf: string): Promise<Client | null>
     abstract save(data: Client): Promise<any>
-    abstract delete(id: string): Promise<Client | undefined>
+    abstract delete(id: string): Promise<any>
 
 }
