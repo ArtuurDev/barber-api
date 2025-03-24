cf94657c54dbc15500eb86d362ac8efd6b5d4bf7
artuur <arturcastrodossantos.com@gmail.com>
2025-03-24 12:49:37 -0300

    entidade de appointments

---
+++ services/barber-shop-service-api/src/domain/enterprise/entities/appointments.ts
@@ -0,0 +1,14 @@
+export interface Appointmentsprops {
+    barber_id: string
+    client_id: string
+    services_id: string[]
+    appointmentDate: Date
+    schedulles_id: string[]
+    amount: number
+    createdAt: Date
+}
+
+
+export class Appointments {
+
+}
