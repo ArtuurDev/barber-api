68a56923a4781a1d29373f141b9beb2797419979
artuur <arturcastrodossantos.com@gmail.com>
2025-03-24 12:50:16 -0300

    entidade de schedulles

---
+++ services/barber-shop-service-api/src/domain/enterprise/entities/schedulles.ts
@@ -0,0 +1,10 @@
+export interface SchedullingProps {
+    isSaturday: boolean
+    InitialSchedulling: string
+    FinalishSchedulling: string
+    active: boolean
+
+}
+export class Schedulling {
+
+}
