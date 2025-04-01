ef048d9e4f0b4aaa54e57c209591132128d90273
artuur <arturcastrodossantos.com@gmail.com>
2025-04-01 13:51:16 -0300

    teste do subscriber

---
+++ services/barber-shop-service-api/src/domain/notification/application/subscribers/on-client-created.spec.ts
@@ -0,0 +1,16 @@
+import { describe, it } from "vitest";
+import { OnClientCreated } from "./on-client-created";
+import { makeClient } from "services/barber-shop-service-api/test/factories/make-client";
+import { ClientRepository } from "../../../application/repositories/client-repositorie";
+import { ClientInMemoryRepository } from "services/barber-shop-service-api/test/repositories/client-in-memory-repository";
+
+describe('on answer Created', () => {
+
+    it('shoul send a notification when an client is created', () => {
+        const sub = new OnClientCreated()
+        const client = makeClient()
+        const repository = new ClientInMemoryRepository()
+        repository.create(client)
+    })
+
+})
