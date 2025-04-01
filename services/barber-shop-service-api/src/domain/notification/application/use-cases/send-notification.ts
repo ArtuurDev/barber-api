7cd56dd66f87dc453d302d9f2975a89d6e88a26d
artuur <arturcastrodossantos.com@gmail.com>
2025-04-01 13:51:40 -0300

    caso de uso de envio de notificação

---
+++ services/barber-shop-service-api/src/domain/notification/application/use-cases/send-notification.ts
@@ -0,0 +1,28 @@
+import { right } from "services/barber-shop-service-api/src/core/either"
+import { Notification, NotificationProps } from "../../enterprise/entities/notification"
+import { NotificationRepository } from "../reposiotories/notifications-repository"
+
+export interface SendNotificationRequest {
+    recipientID: string
+    title: string
+    content: string
+}
+
+export class SendNotificationUseCase {
+    constructor(private readonly notificationRepository:NotificationRepository ) {
+
+    }
+
+    async execute({content,recipientId,title}: NotificationProps) {
+
+        const notification = Notification.create({
+            content,
+            recipientId,
+            title            
+        })
+
+        return right(notification)
+
+    }
+
+}
