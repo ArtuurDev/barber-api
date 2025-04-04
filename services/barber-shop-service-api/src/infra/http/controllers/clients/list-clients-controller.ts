936a1cddca8c1c1f751a103d6427c7836e02d2da
artuur <arturcastrodossantos.com@gmail.com>
2025-04-04 02:40:18 -0300

    refactor

--- services/barber-shop-service-api/src/infra/http/controllers/clients/list-clients-controller.ts
+++ services/barber-shop-service-api/src/infra/http/controllers/clients/list-clients-controller.ts
@@ -10,16 +10,16 @@ export class GetClientsController {
 
     }
 
-    @Get('clients')
+    @Get()
     async handle(@Res() res: Response) {
 
-        const {value} = await this.listClientsUseCase.execute()
+        const {value, isLeft} = await this.listClientsUseCase.execute()
     
-        if(!value) {
+        if(isLeft()) {
             return res.send(value)
         }
 
-       const clients = value.map(GetListClientPresenter.toHttp)
+       const clients = value.clients.map(GetListClientPresenter.toHttp)
 
        return res.send({
         clients: clients
