8f0a6ee3a716afbd76a6e16dd810d571b7725899
artuur <arturcastrodossantos.com@gmail.com>
2025-04-04 02:47:07 -0300

    caso de uso de authenticaação

---
+++ services/barber-shop-service-api/src/domain/application/use-cases/authenticate-client.ts
@@ -0,0 +1,42 @@
+import { left, right } from "services/barber-shop-service-api/src/core/either";
+import { ClientRepository } from "../repositories/client-repositorie";
+import { EmailFormatIncorretly } from "../../errors/email-format-incorretly";
+import { Injectable } from "@nestjs/common";
+import { AuthenticateError } from "../../errors/authenticate";
+import { PasswordHashRepository } from "../repositories/password-hash-repository";
+
+export interface AuthenticateRequest {
+
+    email: string
+    password: string
+    
+}
+
+@Injectable()
+export class AuthenticateClientUseCase {
+    
+    constructor(
+    private readonly clientRepository: ClientRepository,
+    private readonly passwordHashRepository: PasswordHashRepository
+    ) {} 
+
+    async execute({email, password}: AuthenticateRequest) {
+
+
+        const client = await this.clientRepository.authenticate(email)
+
+        if(!client) {
+            return left(new AuthenticateError())
+        }
+
+        const passordHashed = await this.passwordHashRepository.compare(password, client.password)
+        if(!passordHashed) {
+            return left(new AuthenticateError())
+        }
+
+        return right({
+            client
+        })
+
+    }
+}
