49dcedd4ac5340ebd229f30d91921c652561c2c2
artuur <arturcastrodossantos.com@gmail.com>
2025-04-09 20:39:10 -0300

    deletado arquivos para fazer de nvo

--- services/barber-shop-service-api/src/domain/clients/application/repositories/client-attachments-repository.ts
+++
@@ -1,5 +0,0 @@
-import { ClientAttachments } from "../../enterprise/entities/client-attachments";
-
-export abstract class ClientAttachmentsRepository {
-    abstract findManyByClientId(clientId: string): Promise<ClientAttachments[]>
-}
