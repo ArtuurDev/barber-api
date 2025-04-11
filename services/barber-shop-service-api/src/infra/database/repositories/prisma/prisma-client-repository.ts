dc21d426882e7fcf7983c2f9225681bb48f4f426
artuur <arturcastrodossantos.com@gmail.com>
2025-04-11 16:45:57 -0300

    identação, tentando melhorar

--- services/barber-shop-service-api/src/infra/database/repositories/prisma/prisma-client-repository.ts
+++ services/barber-shop-service-api/src/infra/database/repositories/prisma/prisma-client-repository.ts
@@ -22,24 +22,18 @@ export class PrismaClientRepository implements ClientRepository {
         await this.prisma.client.create({
             data
         })
-        
         const attachments = client.attachments.getItems()
-
         await this.clientAttachmentRepository.createMany(attachments)
-        
     }
 
     async authenticate(email: string ): Promise<PrismaClient | null> {
         
         const client = await this.prisma.client.findFirst({
             where: {
-                email, 
-                
+                email,  
             }
         })
-
         return client
-
     }
 
     async findByPassword(password: string, id: string): Promise<Client | null> {
@@ -50,24 +44,18 @@ export class PrismaClientRepository implements ClientRepository {
                 password
             }
         })
-
         if(!client) {
             return null
         }
-
         return PrismaMapper.toDomain(client)
-
     }
 
     async find(): Promise<Client[] | []> {
 
         const data = await this.prisma.client.findMany()
-
         const clients = data.map(item => PrismaMapper.toDomain(item))
-
         return clients
     }
-
 
     async findById(id: string): Promise<Client | null> {
         
@@ -76,11 +64,9 @@ export class PrismaClientRepository implements ClientRepository {
                 id
             }
         })
-
         if(!client) {
             return null
         }
-
         return PrismaMapper.toDomain(client)
     }
 
@@ -91,27 +77,22 @@ export class PrismaClientRepository implements ClientRepository {
                 email
             }
         })
-
         if(!client) {
             return null
         }
-
         return PrismaMapper.toDomain(client)
     }
 
     async findByCpf(cpf: string): Promise<Client | null> {
         
         const client = await this.prisma.client.findUnique({
-            
             where: {
                 cpf
             }
         })
-
         if(!client) {
             return null
         }
-
         return PrismaMapper.toDomain(client)
     }
 
@@ -122,13 +103,10 @@ export class PrismaClientRepository implements ClientRepository {
                 phone
             }
         })
-
         if(!client) {
             return null
         }
-
         return PrismaMapper.toDomain(client)
-
     }
     
     async save(client: Client) {
@@ -140,7 +118,6 @@ export class PrismaClientRepository implements ClientRepository {
             },
             data
         })
-
     }
 
     async delete(id: string): Promise<any> {
@@ -150,11 +127,8 @@ export class PrismaClientRepository implements ClientRepository {
                 id
             }
         })
-
         if(!client) return null
-
         return client
-
     }
 
 }
