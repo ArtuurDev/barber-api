10a0cfa308e3257f96806b2f62f907604ee85037
artuur <arturcastrodossantos.com@gmail.com>
2025-04-14 15:39:32 -0300

    correção do nome da class

--- services/barber-shop-service-api/src/domain/clients/application/repositories/client-attachment-repository.ts
+++ services/barber-shop-service-api/src/domain/clients/application/repositories/client-attachment-repository.ts
@@ -2,6 +2,6 @@ import { Attachment } from "@prisma/client";
 import { ClientAttachments } from "../../enterprise/entities/client-attachments";
 import { Client } from "../../enterprise/entities/client";
 
-export abstract class CiientAttachmentRepository {
+export abstract class ClientAttachmentRepository {
     abstract createMany(attachments: ClientAttachments[]): Promise<Attachment[]>
 }
