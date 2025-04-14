72af06c376ae679e8fa33ff7136c5a0d32ca9f56
artuur <arturcastrodossantos.com@gmail.com>
2025-04-14 15:44:54 -0300

    atualizações

--- services/barber-shop-service-api/src/infra/http/controllers/admin/list-clients-controller.ts
+++ services/barber-shop-service-api/src/infra/http/controllers/admin/list-clients-controller.ts
@@ -1,15 +1,19 @@
-import { Controller, Get, Res } from "@nestjs/common";
+import { Controller, Get, Res, UseGuards } from "@nestjs/common";
 import { Response } from "express";
 import { GetListClientPresenter } from "../../presenters/get-clients-presenter";
 import { GetClientsUseCase } from "services/barber-shop-service-api/src/domain/clients/application/use-cases/get-clients";
+import { Roles } from "../../auth/enum-role";
+import { AuthGuard } from "../../auth/auth-guard";
 
 @Controller("/clients")
+@UseGuards(AuthGuard)
 export class GetClientsController {
 
-    constructor(private readonly listClientsUseCase: GetClientsUseCase) {
+    constructor(
+    private readonly listClientsUseCase: GetClientsUseCase
+    ) {}
 
-    }
-
+    @Roles(['ADMIN'])
     @Get()
     async handle(@Res() res: Response) {
 
@@ -24,8 +28,6 @@ export class GetClientsController {
        return res.send({
         clients: clients
        })
-        
-      
     }
 
 }
