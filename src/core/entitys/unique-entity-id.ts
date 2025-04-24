cc8bb15ade1f0c5e40a21ba7f6a4f700fa552d10
artuur <arturcastrodossantos.com@gmail.com>
2025-04-24 00:41:43 -0300

    value object id

---
+++ src/core/entitys/unique-entity-id.ts
@@ -0,0 +1,14 @@
+import {randomUUID} from 'node:crypto'
+export class UniqueEntityId {
+
+    private _value: string
+
+    constructor(value?: string) {
+        this._value = value ?? randomUUID()
+    }
+
+    get toValue() {
+        return this._value
+    }
+
+}
