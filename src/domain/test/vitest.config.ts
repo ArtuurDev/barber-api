aca2aaca16c9fe0b4e56bfa6666d6922d1cab9c2
artuur <arturcastrodossantos.com@gmail.com>
2025-04-24 03:34:15 -0300

    tsconfig e vite

---
+++ src/domain/test/vitest.config.ts
@@ -0,0 +1,12 @@
+/// <reference types="vitest" />
+import path from 'path'
+import { defineConfig } from 'vitest/config'
+
+export default defineConfig({
+  test: {
+    globals: true,
+    environment: 'node',
+    setupFiles: './vitest.setup.ts', 
+  }
+  }
+)
