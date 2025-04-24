9c5012fedef45cd6e7038f3501ebe73dd3615ae0
artuur <arturcastrodossantos.com@gmail.com>
2025-04-24 00:43:25 -0300

    repositorio do customer em memoria

---
+++ src/domain/test/repositories/in-memory-customer-repository.ts
@@ -0,0 +1,26 @@
+import { CustomerRepository } from "../../barber-shop/customer/application/repositories/customer-repository";
+import { Customer } from "../../barber-shop/customer/enterprise/entities/customer";
+
+export class InMemoryCustomerRepository implements CustomerRepository {
+    
+
+    public items: Customer[] = []
+
+    async create(customer: Customer): Promise<Customer> {
+        this.items.push(customer)
+        return customer
+    }
+
+    async findById(id: string): Promise<Customer | undefined> {
+        return this.items.find(item => item.id.toValue === id)
+    }
+
+    async findByEmail(email: string): Promise<Customer | undefined> {
+        return this.items.find(item => item.email.value === email)
+    }
+
+    async findByCpf(cpf: string): Promise<Customer | undefined> {
+        return this.items.find(item => item.cpf.value === cpf)
+    }
+    
+}
