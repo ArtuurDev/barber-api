11124ed3540bc7fa5a624999fcd7a4cbac030a24
artuur <arturcastrodossantos.com@gmail.com>
2025-04-27 11:51:50 -0300

    repositorio de serviços

---
+++ src/domain/barber-shop/servicesList/application/repositories/service-list-repository.ts
@@ -0,0 +1,6 @@
+import { ServicesList } from "../../enterprise/entities/serviceList";
+
+export abstract class ServiceListRepository {
+    abstract create(serviceList: ServicesList): Promise<any>
+    abstract verifyServicesBelongToBarber(services: string[], barber: string): Promise<boolean>
+}
