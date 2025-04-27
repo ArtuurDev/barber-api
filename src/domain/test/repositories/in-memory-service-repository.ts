cb6916bf9df58c06cd832da3cba90aa8a1e7e2ca
artuur <arturcastrodossantos.com@gmail.com>
2025-04-27 11:52:50 -0300

    repositorio em memoria de serviço

---
+++ src/domain/test/repositories/in-memory-service-repository.ts
@@ -0,0 +1,28 @@
+import { ServiceListRepository } from "src/domain/barber-shop/servicesList/application/repositories/service-list-repository";
+import { ServicesList } from "src/domain/barber-shop/servicesList/enterprise/entities/serviceList";
+
+export class InMemoryServiceRepository implements ServiceListRepository {
+    public items: ServicesList[] = []
+    constructor() {
+        
+    }
+
+    async create(serviceList: ServicesList): Promise<any> {
+        return this.items.push(serviceList)
+    }
+    async verifyServicesBelongToBarber(services: string[], barber: string): Promise<boolean> {
+        
+        // verificando em cada serviço se o id é igual aos que foram passado por parametro, se sim,
+        // se no atributo barber de cada serviço seu id é igual ao parametro barber, add na variavel
+        let servicesOfBarber = 0
+        for(const item of this.items) {
+            if(services.includes(item.id.toValue)) {
+                if(item.barbers.some(barberId => barberId.toValue === barber)) {
+                    servicesOfBarber++
+                }
+            }
+        }
+        // se não for igual, é menor, se for menor, o barber não contém um dos serviços 
+        return servicesOfBarber === services.length
+    }
+}
